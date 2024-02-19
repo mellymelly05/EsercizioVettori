04d94e5854c064ff127df7b303468c2a0f6f7ae8
melis <melis@LAPTOP-N1S6S5M8>
2024-02-19 22:15:02 +0100

    Add project files.

---
+++ EsercizioVettori/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioVettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m1 = 6 * Math.Pow(10, 24);
+            double m2 = 7 * Math.Pow(10, 22);
+            double m3 = m2;
+            double m4 = m2;
+            double G = 6.67 * Math.Pow(10,(-11));
+            double l= 400 * Math.Pow(10,6);
+
+
+            Vettore F12 = new Vettore((G*m1*m2)/Math.Pow(l,2), 0);
+            Vettore F14 = new Vettore(0, (G * m1 * m2) / Math.Pow(l, 2));
+            Vettore F13 = new Vettore(((G * m1 * m2) / Math.Pow(l * Math.Sqrt(2), 2) / Math.Sqrt(2)), (G * m1 * m2) / Math.Pow(l * Math.Sqrt(2), 2) / Math.Sqrt(2));
+            Vettore T=new Vettore(F12.X+F13.X+F14.X,F12.Y+F13.Y+F14.Y);
+
+        }
+    }
+}
---
+++ EsercizioVettori/Vettore.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace EsercizioVettori
+{
+    internal class Vettore
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vettore(double X, double Y)
+        {
+            X = X;
+            Y = Y;
+        }
+
+
+        //somma vettoriale
+        public static Vettore operator +(Vettore a, Vettore b)
+        {
+            return new Vettore(a.X + b.X, a.Y + b.Y);
+        }
+
+        //diferenza tra due vettori
+        public static Vettore operator -(Vettore a, Vettore b)
+        {
+            return new Vettore(a.X - b.X, a.Y - b.Y);
+        }
+
+        //prodotto tra vettore e ubo scalare
+        public static Vettore operator *(Vettore a, double b)
+        {
+            return new Vettore(a.X * b, a.Y * b);
+        }
+
+        //Divisione tra un vettore e uno scalare 
+        public static Vettore operator /(Vettore a, double b)
+        {
+            return new Vettore(a.X / b, a.Y / b);
+        }
+
+        //prodotto scalare 
+        public double ProdottoScalere(Vettore a,Vettore b)
+        {
+           return ((a.X * b.X) + (a.Y * b.Y));
+        }
+
+        public static Vettore operator *(double b,Vettore a)
+        {
+            return new Vettore(a.X * b, a.Y * b);
+        }
+
+        public static Vettore Parse(string s)
+        {
+            string[] a= s.Split(',');
+            double X= double.Parse(a[0]);
+            double Y= double.Parse(a[1]);
+            return new Vettore(X, Y);
+        }
+
+
+    }
+}
